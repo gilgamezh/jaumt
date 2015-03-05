600ff615a45b3bb251db30addaee793f4528bfd6
gilgamezh <mail@gilgamezh.me>
2015-03-04 22:54:25 -0300

    mas cambios en el modelo y esqueleto para celery

--- jaumt/admin.py
+++ jaumt/admin.py
@@ -4,8 +4,18 @@ from django.contrib import admin
 from jaumt.models import Url, Website, RecipientList
 
 
-admin.site.register(Website)
-admin.site.register(Url)
+class WebsiteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'enabled', 'owner')
+    list_filter = ['owner']
+
+
+class UrlAdmin(admin.ModelAdmin):
+    list_display = ('website', 'description', 'url', 'current_status',
+                    'enabled', 'last_check')
+    list_filter = ['website', 'current_status']
+
+admin.site.register(Website, WebsiteAdmin)
+admin.site.register(Url, UrlAdmin)
 admin.site.register(RecipientList)
 
 
--- jaumt/models.py
+++ jaumt/models.py
@@ -2,22 +2,23 @@ from django.db import models
 from django.contrib.auth.models import Group, User
 
 
-class Website(models.Model):
-    name = models.CharField(max_length=30, unique=True)
-    description = models.CharField(max_length=140)
-    enabled = models.BooleanField(default=False, blank=True)
-    owner = models.ForeignKey(User)
-
-    def __str__(self):
-        return self.name
-
-
 class RecipientList(models.Model):
     description = models.CharField(max_length=300)
     recipients = models.ManyToManyField(Group)
 
     def __str__(self):
         return self.description
+
+
+class Website(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=140)
+    enabled = models.BooleanField(default=False, blank=True)
+    owner = models.ForeignKey(User)
+    recipients_list = models.ManyToManyField(RecipientList)
+
+    def __str__(self):
+        return self.name
 
 
 class Url(models.Model):
@@ -40,24 +41,30 @@ class Url(models.Model):
     no_cache = models.BooleanField(default=False, blank=True)
     match_text = models.CharField(max_length=100, null=True, blank=True)
     no_match_text = models.CharField(max_length=100, null=True, blank=True)
-    recipients_list = models.ManyToManyField(RecipientList)
+    recipients_list = models.ManyToManyField(RecipientList,
+                                             blank=True,
+                                             null=True)
+
     enabled = models.BooleanField(default=False, blank=True)
+
     # not editables
     current_status = models.IntegerField(choices=STATUS_CHOICES,
                                          default=OK, editable=False)
+    last_check = models.DateTimeField(null=True,
+                                      editable=False,
+                                      auto_now_add=True)
+
     last_check_ok = models.DateTimeField('Last OK',
                                          null=True,
                                          editable=False)
+
     last_check_warn = models.DateTimeField('Last WARNING',
                                            null=True,
                                            editable=False)
+
     last_check_error = models.DateTimeField('Last ERROR',
                                             null=True,
                                             editable=False)
 
     def __str__(self):
-        if self.hostname is None:
-            return self.url
-        else:
-            return "{} - Hostname: {}".format(self.url, self.hostname)
-
+        return "{} - {}".format(self.url, self.hostname)
