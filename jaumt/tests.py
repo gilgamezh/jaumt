fc9f04f6fc2c78697a15d1e39f9bc78e42eb57c2
gilgamezh <mail@gilgamezh.me>
2015-03-25 22:18:26 -0300

    send_alert tests

--- jaumt/tests.py
+++ jaumt/tests.py
@@ -17,20 +17,46 @@
 
 import requests
 import requests_mock
-
-from django.test import TestCase
 from unittest.mock import MagicMock
 
-from jaumt.models import Url
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+
+from jaumt.models import Url, Website, RecipientList
 
 
 class UrlTestCase(TestCase):
     """ Tests for the Url model. """
+    # users
+    def setUp(self):
+        self.monesvol = User.objects.create_user('monesvol', email='monesvol@jaumt.com')
+        self.pirate = User.objects.create_user('pirate', email='pirate@jaumt.com')
+        self.gilgamezh = User.objects.create_user('gilgamezh', email='gilgamezh@jaumt.com')
+        #recipient lists
+        self.recipient_list1 = RecipientList.objects.create(description='test recipient list 1')
+        self.recipient_list1.recipients.add(self.monesvol)
+        self.recipient_list1.recipients.add(self.pirate)
+        self.recipient_list2 = RecipientList.objects.create(description='test recipient list 2')
+        self.recipient_list2.recipients.add(self.pirate)
+        self.recipient_list2.recipients.add(self.gilgamezh)
+        # websites
+        self.test_site1 = Website.objects.create(name='testsite', description='test site',
+                                                 owner=self.gilgamezh)
+        self.test_site1.recipients_list.add(self.recipient_list1)
+        self.test_site2 = Website.objects.create(name='testsite2', description='test site 2',
+                                                 owner=self.pirate)
+        self.test_site2.recipients_list.add(self.recipient_list2)
+        #  settings
+        settings.JAUMT_EMAIL_FROM =  'jaumt@jaumt.com'
+        settings.JAUMT_EMAIL_PREFIX = '[Jaumt]'
+        settings.JAUMT_USER_AGENT = "User-Agent': 'Jaumt/0.1 (+http://jaumt.com)"
 
     #  Url.update_status() tests
     def test_url_status_ok_error(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  status='OK',
                                  enabled=True)
@@ -41,7 +67,7 @@ class UrlTestCase(TestCase):
 
     def test_url_status_ok_ok(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  status='OK',
                                  enabled=True)
@@ -52,7 +78,7 @@ class UrlTestCase(TestCase):
 
     def test_url_status_warning_error(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  status='WARNING',
                                  enabled=True)
@@ -63,7 +89,7 @@ class UrlTestCase(TestCase):
 
     def test_url_status_warning_ok(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  status='WARNING',
                                  enabled=True)
@@ -74,7 +100,7 @@ class UrlTestCase(TestCase):
 
     def test_url_status_downtime_error(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  status='DOWNTIME',
                                  enabled=True)
@@ -85,7 +111,7 @@ class UrlTestCase(TestCase):
 
     def test_url_status_downtime_ok(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  status='DOWNTIME',
                                  enabled=True)
@@ -96,7 +122,7 @@ class UrlTestCase(TestCase):
 
     def test_url_status_retrying_error(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  status='RETRYING',
                                  enabled=True)
@@ -107,7 +133,7 @@ class UrlTestCase(TestCase):
 
     def test_url_status_retrying_ok(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  status='RETRYING',
                                  enabled=True)
@@ -119,7 +145,7 @@ class UrlTestCase(TestCase):
     #  Url.handle_response() tests
     def test_url_handle_response_with_error(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  enabled=True)
         url.update_status = MagicMock()
@@ -128,7 +154,7 @@ class UrlTestCase(TestCase):
 
     def test_url_handle_response_ok(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  match_text='',
                                  no_match_text='',
@@ -144,7 +170,7 @@ class UrlTestCase(TestCase):
 
     def test_url_handle_response_error(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  match_text='',
                                  no_match_text='',
@@ -160,7 +186,7 @@ class UrlTestCase(TestCase):
 
     def test_url_handle_response_match_text_ok(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  match_text='foo',
                                  no_match_text='',
@@ -176,7 +202,7 @@ class UrlTestCase(TestCase):
 
     def test_url_handle_response_match_text_error(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  match_text='foo',
                                  no_match_text='',
@@ -192,7 +218,7 @@ class UrlTestCase(TestCase):
 
     def test_url_handle_response_no_match_text_error(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  match_text='',
                                  no_match_text='foo',
@@ -208,7 +234,7 @@ class UrlTestCase(TestCase):
 
     def test_url_handle_response_no_match_text_ok(self):
         url = Url.objects.create(description='test site 1',
-                                 website_id=1,
+                                 website=self.test_site1,
                                  url='http://example.com',
                                  match_text='',
                                  no_match_text='foo',
@@ -221,4 +247,39 @@ class UrlTestCase(TestCase):
         url.update_status = MagicMock()
         url.handle_response(response=response)
         url.update_status.assert_called_with(False, '200')
+
+    #test send_alerts()
+    def test_send_ok_alert(self):
+        url = Url.objects.create(description='test site 1',
+                                 website=self.test_site1,
+                                 url='http://example.com',
+                                 status='RETRYING',
+                                 match_text='',
+                                 no_match_text='',
+                                 current_status_code='TestCase',
+                                 enabled=True)
+        send_mail = MagicMock()
+        url.send_alerts()
+        subject = '[Jaumt][OK] test site 1'
+        message = ("""Current status for test site 1 is: OK \n\r
+                   Url: http://example.com \n\r
+                   Current http_status or error message: TestCase \n\r
+                   Last Check: {} \n\r
+                   Last Check OK: {} \n\r
+                   Url comments: \n\r
+                   {}""").format(self.description, current_status, self.current_status_code,
+                                 self.url, self.last_check, self.last_check_ok, self.alert_footer)
+
+
+    def test_send_ok_alert_with_rcplist(self):
+        url = Url.objects.create(description='test site 1',
+                                 website=self.test_site1,
+                                 url='http://example.com',
+                                 status='RETRYING',
+                                 match_text='',
+                                 no_match_text='foo',
+                                 recipients_list=self.recipient_list1,
+                                 enabled=True)
+
+
 
